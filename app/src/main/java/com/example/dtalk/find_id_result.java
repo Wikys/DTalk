b7855836304bf0d523043f8fc075a240d952a18d
ayc0812 <ayc0812@gmail.com>
2023-09-14 02:17:44 +0900

    프래그먼트 만들차례

---
+++ app/src/main/java/com/example/dtalk/find_id_result.java
@@ -0,0 +1,14 @@
+package com.example.dtalk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class find_id_result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_id_result);
+    }
+}
---
+++ app/src/main/java/com/example/dtalk/find_ps.java
@@ -0,0 +1,14 @@
+package com.example.dtalk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class find_ps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_ps);
+    }
+}
