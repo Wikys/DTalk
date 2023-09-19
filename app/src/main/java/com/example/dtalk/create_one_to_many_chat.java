ec56c2e586881ca368a2bc2f958bcca3b6f621ef
ayc0812 <ayc0812@gmail.com>
2023-09-20 04:18:33 +0900

    모임채팅목록 화면

---
+++ app/src/main/java/com/example/dtalk/create_one_to_many_chat.java
@@ -0,0 +1,14 @@
+package com.example.dtalk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class create_one_to_many_chat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_one_to_many_chat);
+    }
+}
